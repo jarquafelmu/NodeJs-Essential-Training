813fb37fec513ff029d5c3730f5c54f4454e8f0e
jarquafelmu <thechaoselement@gmail.com>
2020-09-29 19:30:53 -0600

    setup interval

--- student/ch03/03_07/finished/timers.js
+++ student/ch03/03_07/finished/timers.js
@@ -1,6 +1,15 @@
 const waitTime = 3000;
+const waitInterval = 500;
+let currentTime = 0;
+
+const incTime = () => {
+    currentTime += waitInterval;
+    console.log(`waiting ${currentTime / 1000} seconds`);
+}
+
 console.log(`setting a ${waitTime / 1000} second delay`);
 
 const timerFinished = () => console.log(`done`);
 
+setInterval(incTime, waitInterval);
 setTimeout(timerFinished, waitTime);
