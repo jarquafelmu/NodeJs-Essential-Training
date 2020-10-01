8e13fecf67011a14a3260d7b366a838c0d63f196
jarquafelmu <thechaoselement@gmail.com>
2020-09-30 19:50:04 -0600

    handling events

--- student/ch04/04_06/finished/events.js
+++ student/ch04/04_06/finished/events.js
@@ -6,6 +6,11 @@ emitter.on(`customEvent`, (message, user) => {
     console.log(`${user}: ${message}`);
 })
 
-
-emitter.emit(`customEvent`, `Hello World`, `Computer`);
-emitter.emit(`customEvent`, `That's pretty cool huh?`, `Alex`);
+process.stdin.on(`data`, data => {
+    const input = data.toString().trim();
+    if (input === `exit`) {
+        emitter.emit(`customEvent`, `Goodbye!`, `process`);
+        process.exit();
+    }
+    emitter.emit(`customEvent`, input, `terminal`);
+})
