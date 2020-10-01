b6be14c00bbc82919372761039b9f270438057b0
jarquafelmu <thechaoselement@gmail.com>
2020-09-30 20:20:17 -0600

    async read img file

--- student/ch05/05_02/finished/readFile.js
+++ student/ch05/05_02/finished/readFile.js
@@ -1,6 +1,6 @@
 const fs = require(`fs`);
 
-fs.readFile(`./assets/Readme.md`, `utf8`, (err, text) => {
+fs.readFile(`./assets/alex.jpg`, (err, img) => {
     console.log(`file contents read`);
-    console.log(text);
+    console.log(img);
 });
