3b5b33c114a17ee5e70fe03a1ad858df0ee25284
jarquafelmu <thechaoselement@gmail.com>
2020-09-30 20:21:24 -0600

    error handling

--- student/ch05/05_02/finished/readFile.js
+++ student/ch05/05_02/finished/readFile.js
@@ -1,6 +1,10 @@
 const fs = require(`fs`);
 
-fs.readFile(`./assets/alex.jpg`, (err, img) => {
+fs.readFile(`./assets/alex.pg`, (err, img) => {
+    if (err) {
+        console.log(`An error has occured: ${err.message}`);
+        process.exit();
+    }
     console.log(`file contents read`);
     console.log(img);
 });
