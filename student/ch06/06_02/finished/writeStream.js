9cd50cecc0f5d72d5968988fd4d356b129ea9c10
jarquafelmu <thechaoselement@gmail.com>
2020-10-03 07:55:10 -0600

    copy file using read and write stream

--- student/ch06/06_02/finished/writeStream.js
+++ student/ch06/06_02/finished/writeStream.js
@@ -3,4 +3,4 @@ const fs = require(`fs`);
 const writeStream = fs.createWriteStream(`./assets/myFile.txt`, `utf8`);
 const readStream = fs.createReadStream(`./assets/lorum-ipsum.md`, `utf8`);
 
-process.stdin.pipe(writeStream);
+readStream.pipe(writeStream);
