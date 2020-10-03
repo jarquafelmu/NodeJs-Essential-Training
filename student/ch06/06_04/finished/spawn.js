1a58854f24a2ef028922e67ec64bb4766e40cd98
jarquafelmu <thechaoselement@gmail.com>
2020-10-03 09:54:00 -0600

    sending info to spawned process

--- student/ch06/06_04/finished/spawn.js
+++ student/ch06/06_04/finished/spawn.js
@@ -2,6 +2,10 @@ const cp = require(`child_process`);
 
 const questionApp = cp.spawn(`node`, [`questions.js`]);
 
+questionApp.stdin.write(`Alex \n`);
+questionApp.stdin.write(`Tahoe \n`);
+questionApp.stdin.write(`Change the world \n`);
+
 questionApp.stdout.on(`data`, data => {
     console.log(`from the question app: ${data}`);
 })
