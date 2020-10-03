fa4c7bc083bdb150fecf278ad40733213da17b85
jarquafelmu <thechaoselement@gmail.com>
2020-10-02 19:57:56 -0600

    remove directory

--- student/ch05/05_07/finished/directories.js
+++ student/ch05/05_07/finished/directories.js
@@ -1,5 +1,11 @@
 const fs = require(`fs`);
 
+// empty the directory
+fs.readdirSync(`./storage`).forEach(fileName => {
+    fs.unlinkSync(`./storage/${fileName}`);
+});
+
+// remove the directory
 fs.rmdir(`./storage`, err => {
     if (err) throw err;
     console.log(`./storage directory removed`)
