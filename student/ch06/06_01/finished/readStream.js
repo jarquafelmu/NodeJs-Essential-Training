66ad3e1ed54348e36890bd42d6e760085829f5c0
jarquafelmu <thechaoselement@gmail.com>
2020-10-02 20:08:10 -0600

    reading file in chunks

--- student/ch06/06_01/finished/readStream.js
+++ student/ch06/06_01/finished/readStream.js
@@ -1,4 +1,8 @@
-console.log("type something...")
-process.stdin.on(`data`, data => {
+const fs = require('fs');
+
+const readStream = fs.createReadStream(`./assets/lorum-ipsum.md`, `utf8`);
+
+console.log("type something...");
+readStream.on(`data`, data => {
     console.log(`I read ${data.length - 1} characters of text`);
 })
